7acdf5b9f3b78a1bd68da50e93692d110418aeea
Camila <camramirez0594@gmail.com>
2022-05-11 15:25:44 -0300

    first commit

---
+++ Item.js
@@ -0,0 +1,16 @@
+export const ropa = [
+    {id: 1, img:"../img/pantalon-1.jpg", nombre:"Jeans New York", tela:"Jeans", tipo: "Pantalón", coleccion: "primavera-verano", precio: 1500, stock: 200},
+    {id: 2, img:"../img/pantalon-2.jpg", nombre:"Jeans Los Ángeles", tela:"Jeans", tipo: "Pantalón", coleccion: "primavera-verano", precio: 1500, stock: 200},
+    {id: 3, img:"../img/remera-1.jpg", nombre:"Remera Corazón", tela:"Algodón", tipo: "Remera", coleccion: "primavera-verano", precio: 1500, stock: 200},
+    {id: 4, img:"../img/remera-2.jpg", nombre:"Remera Guns", tela:"Algodón y Lycra", tipo: "Remera", coleccion: "primavera-verano", precio: 1500, stock: 200},
+    {id: 5, img:"../img/calza-1.jpg", nombre:"Calza 3/4", tela:"Lycra", tipo: "Calza", coleccion: "primavera-verano", precio: 1500, stock: 200},
+    {id: 6, img:"../img/calza-2.jpg", nombre:"Calza Neón", tela:"Lycra", tipo: "Calza", coleccion: "primavera-verano", precio: 1500, stock: 200},
+    {id: 7, img:"../img/vestido-1.jpg", nombre:"Vestido de verano", tela:"", tipo: "Vestido", coleccion: "primavera-verano", precio: 1500, stock: 200},
+    {id: 8, img:"../img/pantalon-3.jpg", nombre:"Jeans Boston", tela:"Jeans", tipo: "Pantalón", coleccion: "otoño-invierno", precio: 1500, stock: 200},
+    {id: 9, img:"../img/pantalon-4.jpg", nombre:"Jeans California", tela:"Jeans", tipo: "Pantalón", coleccion: "otoño-invierno", precio: 1500, stock: 200},
+    {id: 10, img:"../img/remera-3.jpg", nombre:"Remera Corazón Rosa", tela:"Algodón", tipo: "Remera", coleccion: "otoño-invierno", precio: 1500, stock: 200},
+    {id: 11, img:"../img/remera-4.jpg", nombre:"Remera Clásica", tela:"Algodón y Lycra", tipo: "Remera", coleccion: "otoño-invierno", precio: 1500, stock: 200},
+    {id: 12, img:"../img/calza-3.jpg", nombre:"Calzas Floreadas", tela:"Lycra", tipo: "Calza", coleccion: "otoño-invierno", precio: 1500, stock: 200},
+    {id: 13, img:"../img/calza-4.jpg", nombre:"Calzas Selva", tela:"Lycra", tipo: "Calza", coleccion: "otoño-invierno", precio: 1500, stock: 200},
+    {id: 14, img:"../img/vestido-2.jpg", nombre:"Vestido de otoño", tela:"Algodón", tipo: "Vestido", coleccion: "otoño-invierno", precio: 1500, stock: 200}
+]
